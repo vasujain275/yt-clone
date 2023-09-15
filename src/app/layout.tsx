bcf7bc9d2c450972974fd928cdc383c703825fa1
Luffy <luffy@localhost.localdomain>
2023-09-15 19:19:51 +0530

    feat: Added NextAuth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { AppBar } from "@/components/AppBar";
+import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
